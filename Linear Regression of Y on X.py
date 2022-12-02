0cbdcb0145016882138e4587f2ad18fbf98f9d86
cxxlwrld <69635062+cxxlwrld@users.noreply.github.com>
2022-12-02 09:17:12 +0300

    Update Linear Regression of Y on X.py

--- Linear Regression of Y on X.py
+++ Linear Regression of Y on X.py
@@ -1,12 +1,9 @@
+mport numpy as np
+
 def regressionLine(x, y):
-    import numpy as np
-    x_squares = np.square(x)
-    xy_product = np.array(x) * np.array(y)
-    x_sum = np.sum(np.array(x))
-    y_sum = np.sum(np.array(y))
-    x_squares_sum = np.sum(x_squares)
-    xy_product_sum = np.sum(xy_product)
-    n = len(x)
-    b = (n * (xy_product_sum) - (x_sum * y_sum)) / ( n * (x_squares_sum) - np.square(x_sum))
-    a = (y_sum - b * x_sum ) / n
-    return  (np.around(a, 4), np.around(b, 4))
+    """ Return the a (intercept)
+        and b (slope) of Regression Line 
+        (Y on X).
+    """
+    a,b = np.polyfit(x,y,1)
+    return round(b,4),round(a,4)

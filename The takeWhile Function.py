97aaf698db82948794683c66cd7e6b22971a48e8
Kelvin Kamau <69635062+cxxlwrld@users.noreply.github.com>
2023-05-25 10:54:22 +0300

    Update The takeWhile Function.py

--- The takeWhile Function.py
+++ The takeWhile Function.py
@@ -1,1 +1,9 @@
-
+def take_while(arr, pred_fun):
+    # TODO: Do your best !
+    result = []
+    for item in arr:
+        if pred_fun(item):
+            result.append(item)
+        else:
+            break
+    return result
